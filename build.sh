562f813886d028f3a47a62ae561f86551cff914d
Umut Işık <umutphp@gmail.com>
2019-01-17 00:00:37 +0300

    Add build shell script

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+## declare an array variable
+declare -a array=("5.6" "7.0" "7.1" "7.2" "7.3")
+
+# get length of an array
+arraylength=${#array[@]}
+
+# use for loop to read all values and indexes
+for (( i=1; i<${arraylength}+1; i++ ));
+do
+    echo "********************************************************"
+    echo "Building image php:" ${array[$i-1]} " - " $i " of " ${arraylength}
+    echo "********************************************************"
+    cd images/${array[$i-1]}/
+    docker build -t umutphp/php-docker-images-for-ci:${array[$i-1]} .
+    docker push umutphp/php-docker-images-for-ci:${array[$i-1]}
+    cd -
+    echo "********************************************************"
+    echo "Image is pushed to remote"
+    echo "********************************************************"
+done
