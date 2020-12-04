3e7b0b965dcca466162278425d14322e91e0143f
Umut Işık <umutphp@gmail.com>
2020-12-04 18:10:09 +0300

    Add 8.0 version

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## declare an array variable
-declare -a array=("5.6" "7.0" "7.1" "7.2" "7.3" "7.4")
+declare -a array=("5.6" "7.0" "7.1" "7.2" "7.3" "7.4" "8.0")
 
 # get length of an array
 arraylength=${#array[@]}
